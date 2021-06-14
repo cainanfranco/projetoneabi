f4fc3957f85cefcf88b09354f7129880aa4be955
Cainan Franco <74746126+cainanfranco@users.noreply.github.com>
2021-06-14 11:55:41 -0300

    Update conexao.php

--- lo/admin/conexao.php
+++ lo/admin/conexao.php
@@ -1,7 +1,7 @@
 <?php
-define('HOST','localhost');
+define('HOST','177.11.49.134');
 define('USUARIO','nandevsi_ifce');
 define('SENHA','cainan55085');
 define('DB','nandevsi_ifce');
-$conexao = mysqli_connect(HOST, USUARIO, SENHA, DB) or die('Não foi possicel conectar');
+$conexao = mysqli_connect(HOST, USUARIO, SENHA, DB) or die('N達o foi possicel conectar');
 ?>
